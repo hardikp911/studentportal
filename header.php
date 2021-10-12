0dcb15e4b2f6b22f4fc723870c3988b49c3f6d33
HARDIK PARMAR <63928268+emeraldnightmare@users.noreply.github.com>
2021-10-12 16:45:06 +0530

    ass

---
+++ header.php
@@ -0,0 +1,197 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <link rel="stylesheet" href="/css/neu.css">
+    <link rel="apple-touch-icon" href="/uploads/cropped-logo.png">
+    
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" media="(prefers-color-scheme: light)" content="white">
+    <meta name="title" content="Portal">
+<meta name="description" content="PortalBank of data,education">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://pwfree.github.io/">
+<meta property="og:title" content="Portal">
+<meta property="og:description" content="PortalBank of data,education">
+<meta property="og:image" content="/uploads/cropped-logo.png">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://pwfree.github.io/">
+<meta property="twitter:title" content="Portal">
+<meta property="twitter:description" content="PortalBank of data,education">
+<meta property="twitter:image" content="/uploads/cropped-logo.png">
+    
+    
+        <!-- font awesome -->
+    <style>
+    header, main, footer {
+        padding-left: 300px;
+    }
+    
+    @media only screen and (max-width: 992px){
+    header, main, footer {
+        padding-left: 0px;
+    }
+    }
+    
+    
+    
+    .docs-footer {
+        margin-top: 40px;
+        background-color: transparent!important;
+        border-top: 1px solid rgba(0,0,0,0.14);
+        color: inherit;
+    }
+
+    object#front-page-logo {
+        width: 280px;
+        margin-left: -26px;
+    }
+
+
+    ul.sidenav.sidenav-fixed li.logo {
+        text-align: center;
+        margin-top: 32px;
+        margin-bottom: 136px;
+    }
+        
+        
+.neu {
+  padding: 2em;
+  border-radius: 1em;
+  border: none;
+  background-color: #eaeaea;
+  outline: none;
+  transition: all 0.1s ease;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.15), -5px -5px 10px white;
+}
+        
+.neu:active {
+  box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.15), inset -5px -5px 10px white;
+}
+
+
+    
+    </style>
+        <!--Import Google Icon Font-->
+        <link href="/css/mat_icon.css" rel="stylesheet">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="/css/mat.css">
+        <!-- icon @ favicon -->
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json">
+  <link rel="stylesheet" href="https://cdn.plyr.io/3.6.8/plyr.css" />
+  <link
+    href="https://fonts.googleapis.com/css?family=Poppins|Quattrocento+Sans"
+    rel="stylesheet"
+  />
+  <script src="https://cdn.plyr.io/3.6.8/plyr.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/hls.js"></script>
+<!--   <link rel="stylesheet" href="https://cdn.plyr.io/3.6.8/demo.css" />
+ -->
+
+        <title>Student Portal</title>
+       
+    </head>
+
+
+
+
+<body>
+
+    <div class="progress" style="margin:0!important">
+        <div class="indeterminate"></div>
+    </div>
+
+<header>
+    <nav class="white">
+
+        <ul id="slide-out" class="sidenav sidenav-fixed">
+            <li>
+            <li><a style="display:none;" class="sidenav-close" href="#!"></a></li>
+            </li>
+
+            <li class="logo"><a id="logo-container" href="/" class="brand-logo">
+        <object id="front-page-logo" type="image/svg+xml" data="/assets/pw.svg" alt="Your browser does not support SVG">Your browser does not support SVG</object></a></li>
+        <li class="divider" tabindex="-1"></li>
+
+            <li><a tabindex="-5" href="/" class="supera brand" style="margin-left:-10px"><i class="material-icons">home</i>Home</a></li>
+            <li class="divider" tabindex="-1"></li>
+
+            
+          
+            
+     <li class="no-padding">
+      <ul class="collapsible collapsible-accordion">
+        <li>
+          <a class="collapsible-header waves-effect waves-teal"><i class="material-icons">golf_course</i>Lakshya JEE HD+ 2021 </a>
+          <div class="collapsible-body">
+            <ul>
+
+              <li class="no-padding">
+                <ul class="collapsible collapsible-accordion">
+                  <li>
+                    <a class="collapsible-header waves-effect waves-teal"><i class="material-icons">school</i>Lakshya JEE 1.0</a>
+                    <div class="collapsible-body">
+                      <ul>
+                        <li><a href="/lakshyabatchnew/physics">Physics</a></li>
+                        <li><a href="/lakshyabatchnew/maths">Maths</a></li>
+                        <li><a href="/lakshyabatchnew/biology">Biology</a></li>
+                        <li><a href="/lakshyabatchnew/chemistery">Chemistery</a></li>
+
+
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+                      
+
+
+
+
+               
+             </ul>
+           </div>
+         </li>
+       </ul>
+     </li>
+
+
+
+            <!-- <li><a class="supera brand" href="view_teacher.php">View Teachers</a> </li> -->
+
+
+            <li class="divider" tabindex="-1"></li>
+
+        </ul>
+
+        <a  tabindex="-5" href="#" data-target="slide-out" class="sidenav-trigger show-on-medium-and-down"><i class="material-icons brand">menu</i></a>
+     
+     
+        <div class="nav-wrapper" style="width:95% ;margin-left:20px;">
+            <a tabindex="-5" href="/" class="brand-logo brand">
+            Physics Wallah            
+            </a>
+             </div>
+    </nav>
+
+</header>
+
+
+    <div id="content">
+            <main  id="mainContent">
+
+
+
+
+
+
