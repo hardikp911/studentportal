ce3f15bd64fdfe571d798f45d46dbafa8669e06d
HARDIK PARMAR <63928268+emeraldnightmare@users.noreply.github.com>
2022-07-01 17:35:16 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,12 +1,16 @@
 <?php
 error_reporting(0);
-   $host = 'localhost';
-   $dbname = 'student_portal';
-   $dbpassword = '';
-   $dbuser = 'root';
+   $host = 'anqe8cs5ccj9.ap-south-2.psdb.cloud';
+   $dbname = 'student_database';
+   $dbpassword = 'pscale_pw_UG9qS4r0i3ueFVou8QkfCRZajenztwNDiDB8dUdQPF0';
+   $dbuser = '99lmdzna9tlr';
+  
+  $connection = mysqli_init();
+  $connection->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $connection->real_connect($host,$dbuser,$dbpassword,$dbname);
+//   $connection->close();
 
-    $connection = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
-
+   
     // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " + mysqli_connect_error();
