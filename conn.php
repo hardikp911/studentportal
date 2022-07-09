6623a995a58e9f071ac6c0cda26dcd3baa5491d2
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-07-09 16:15:03 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,10 @@
 <?php
 error_reporting(1);
-   $host = 'ypty5a7p9v40.us-east-1.psdb.cloud';
+$host = 'ypty5a7p9v40.us-east-1.psdb.cloud';
    $dbname = 'somegumtank';
-   $dbpassword = 'pscale_pw_JhnCK9w-7jYTCxmIDYMIJ8C2hKQUGPy5QL6m8eIu6Mk';
-   $dbuser = '7u4c8lue2nbp';
+   $dbpassword = 'pscale_pw_y3yGJ7cP5ICkZlEVZjF3CDFJbBr0DV9oZeC6JyjV5Jw';
+   $dbuser = '9p6cb850b5f0';
+
 
   $connection = mysqli_init();
   $connection->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
